4e82a9a8a38676da25d73ece83eed7efacfa5535
SayemTaher <sayemtaher788@gmail.comn>
2024-04-30 18:42:12 +0200

    deploy successfull

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const spotDataCollection = client.db('spotsDB').collection('spots')
         // get the data posted from the client 
         app.post('/addTourists', async (req, res) => {
@@ -122,7 +122,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
