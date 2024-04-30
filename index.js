6eaebde7dc5d205a1e56a4aafd9700b0b7628ec9
SayemTaher <sayemtaher788@gmail.comn>
2024-04-30 18:50:52 +0200

    rechecked network connection

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-// MonoDB Database setup
+// MonoDB Database setup for connection 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vybo3pc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
